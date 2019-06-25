757793afa7078983e51392eee6c1a2b3d0d60eee
IndimaDias <indimad@yahoo.com>
2019-06-25 17:14:09 -0400

    app.post modified to read form the form

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,28 @@ function app(app){
     });
     
     app.post("/api/friends",function(req,res){
-        return console.log("Post");
+        var reqBody = req.body;
+
+        var newFriend = {
+            name : reqBody.yourName,
+            photo: reqBody.link,
+            scores :[reqBody.ques1,
+                    reqBody.ques2,
+                    reqBody.ques3,
+                    reqBody.ques4,
+                    reqBody.ques5,
+                    reqBody.ques6,
+                    reqBody.ques7,
+                    reqBody.ques8,
+                    reqBody.ques9,
+                    reqBody.ques10
+            ]
+        }
+
+        var userScores = newFriend.scores;
+
+        return console.log(userScores);
+    
     });
     
 }
